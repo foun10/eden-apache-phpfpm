d5073715d9b3c6867a28ff01646277a3632365a5
Alexander Schneider <schneider@foun10.de>
2024-07-10 13:58:57 +0200

    Adjust key size

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 if [[ ! -d "/root/ssl" ]]; then
     mkdir /root/ssl;
-    openssl genrsa -out /root/ssl/local.key 1024;
+    openssl genrsa -out /root/ssl/local.key 2048;
     openssl req -new -key /root/ssl/local.key -out /root/ssl/local.csr -subj \
         "/C=${SSL_COUNTRY}/ST=${SSL_STATE}/L=${SSL_CITY}/O=${SSL_COMPANY}/OU=Development/CN=${SSL_HOST}"
     openssl x509 -req -days 365 -in /root/ssl/local.csr -signkey /root/ssl/local.key -out /root/ssl/local.crt
